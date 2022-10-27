95ad6ac7aea13699f86ec31ad9fe03eb4a789430
dpound <dpound@naver.com>
2022-10-27 21:02:44 +0900

    채팅, 경매등 실시간을 담당하는 어플리케이션 , 깃 이그노어와 리드미 한번에 포함

---
+++ src/main/java/com/example/auctionchat/AuctionChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auctionchat/AuctionChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

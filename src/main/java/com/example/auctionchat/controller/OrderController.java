9b843ab97978771463b014ecd9d7d7d3d105a669
dpound <dpound@naver.com>
2023-02-26 23:02:16 +0900

    실시간 채팅 제품 오더 세이브 기능 추가 , 테스트는 안해봄

--- src/main/java/com/example/auctionchat/controller/OrderController.java
+++ src/main/java/com/example/auctionchat/controller/OrderController.java
@@ -1,15 +1,31 @@
 package com.example.auctionchat.controller;
 
+import com.example.auctionchat.models.Order;
+import com.example.auctionchat.mongomodel.OrderModel;
+import com.example.auctionchat.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Log4j2
+@RequestMapping(value = "user/order")
 @RestController
 public class OrderController {
 
+    private final OrderService orderService;
 
+
+    @PostMapping(value = "save")
+    public Mono<ResponseEntity<String>> saveOrder(@RequestBody OrderModel orderModel){
+
+        return orderService.saveOrder(orderModel);
+    }
 
 
 }
--- src/main/java/com/example/auctionchat/service/OrderService.java
+++ src/main/java/com/example/auctionchat/service/OrderService.java
@@ -4,9 +4,12 @@ import com.example.auctionchat.mongomodel.OrderModel;
 import com.example.auctionchat.mongorepository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -18,9 +21,12 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
 
-    public Mono<OrderModel> saveOrder(OrderModel orderModel){
+    public Mono<ResponseEntity<String>> saveOrder(OrderModel orderModel){
 
-        return orderRepository.save(orderModel);
+        return orderRepository
+                .save(orderModel)
+                .map(orderModel1 -> new ResponseEntity<>("success save", HttpStatus.OK))
+                .subscribeOn(Schedulers.single());
     }
 
 
